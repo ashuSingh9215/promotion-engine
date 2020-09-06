56fed642a9697a55b52115b0a536c3a32d97feca
asingh <ashutosh.singh@infobip.com>
2020-09-07 00:14:45 +0530

    Main class

---
+++ src/main/java/com/abc/shopping/Application.java
@@ -0,0 +1,11 @@
+package com.abc.shopping;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String a[]){
+
+    }
+}
